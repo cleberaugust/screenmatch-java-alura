eac4e1731c72220684153151a9052eea8884036b
Cleber <cleber_august@yahoo.com.br>
2026-02-08 11:14:09 -0300

    feat: implementa classes Filme e Serie herdando de Titulo

--- src/br/com/alura/screenmatch/Filme.java
+++ src/br/com/alura/screenmatch/Filme.java
@@ -1,4 +1,13 @@
 package br.com.alura.screenmatch;
 
-public class Filme {
+public class Filme extends Titulo {
+    private String diretor;
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
 }
--- src/br/com/alura/screenmatch/Serie.java
+++ src/br/com/alura/screenmatch/Serie.java
@@ -1,4 +1,45 @@
 package br.com.alura.screenmatch;
 
-public class Serie {
+public class Serie extends Titulo{
+    private int temporadas;
+    private boolean ativa;
+    private int episodiosPorTemporadas;
+    private int minutosPorEpisodios;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getEpisodiosPorTemporadas() {
+        return episodiosPorTemporadas;
+    }
+
+    public void setEpisodiosPorTemporadas(int episodiosPorTemporadas) {
+        this.episodiosPorTemporadas = episodiosPorTemporadas;
+    }
+
+    public int getMinutosPorEpisodios() {
+        return minutosPorEpisodios;
+    }
+
+    public void setMinutosPorEpisodios(int minutosPorEpisodios) {
+        this.minutosPorEpisodios = minutosPorEpisodios;
+    }
+
+    @Override
+    public int getDuracaoEmMinutos() {
+        return temporadas * episodiosPorTemporadas * minutosPorEpisodios;
+    }
 }
