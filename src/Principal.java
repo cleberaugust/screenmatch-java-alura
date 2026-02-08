fe213fae399bca5d495bb2ebf5b15f5b42ebebe3
Cleber <cleber_august@yahoo.com.br>
2026-02-08 11:29:35 -0300

    feat: implementa testes iniciais na classe Principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,15 @@
 import br.com.alura.screenmatch.Filme;
+import br.com.alura.screenmatch.Serie;
 
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "Poderoso Chefão";
-        meuFilme.anoDeLancamento = 1970;
+        meuFilme.setNome("Poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
+
+
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(5);
@@ -13,6 +18,15 @@ public class Principal {
         System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacao());
         System.out.println(meuFilme.pegaMedia());
 
+        Serie lost = new Serie();
+
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporadas(10);
+        lost.setMinutosPorEpisodios(50);
+        System.out.println("Duração para maratonar lost: " + lost.getDuracaoEmMinutos());
 
     }
 }
